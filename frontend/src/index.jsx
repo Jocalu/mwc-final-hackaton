d6ecc31065763699d16eeecf2c5f3e52650151e1
Jocalu <jocalu87@gmail.com>
2021-06-29 13:28:08 +0200

    Add login Add global styles Add router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/scss/styles.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hola mundo</h1>
+    <Router>
+      <Switch>
+        <Route exact path="/"><Login /></Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
