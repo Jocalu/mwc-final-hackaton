4d3d65f1c89e20a9af90a5c2042c22ec177d6419
Jocalu <jocalu87@gmail.com>
2021-06-29 13:28:08 +0200

    Add login Add global styles Add router

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,92 @@
+import './Login.scss';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { useState } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const [values, setValues] = useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <main className="login">
+      <div className="login__wrapper shadow br ml-8">
+        <h1 className="mb-8">Login</h1>
+
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          helperText="Incorrect entry."
+        />
+        <br />
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            helperText="Incorrect entry."
+            endAdornment={(
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            )}
+            labelWidth={70}
+          />
+        </FormControl>
+        <br />
+        <br />
+        <Button variant="contained" color="primary" size="large">
+          ENTRAR
+        </Button>
+
+        <span className="login__message text--grey mt-4">
+          ¿No tienes cuenta?
+          <Link
+            className="ml-1"
+            style={{ textDecoration: 'none', color: '#0361b7' }}
+            to="/register"
+          >
+            Regístrate
+
+          </Link>
+        </span>
+      </div>
+    </main>
+  );
+}
+
+export default Login;
