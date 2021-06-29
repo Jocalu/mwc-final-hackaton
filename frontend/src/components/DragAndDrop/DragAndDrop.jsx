ad8f4a50136bc17fab5e4379266ef3f9056deded
Jocalu <jocalu87@gmail.com>
2021-06-29 16:54:02 +0200

    Fix drop

--- frontend/src/components/DragAndDrop/DragAndDrop.jsx
+++ frontend/src/components/DragAndDrop/DragAndDrop.jsx
@@ -2,30 +2,41 @@ import React, {useCallback, useContext, useEffect} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { FilesContext } from '../../context/FilesContext';
 import './DragAndDrop.scss'
+import { useHistory } from "react-router-dom";
 
 function DragAndDrop() {
+    const history = useHistory();
 
     const {data, setData, readFiles} = useContext(FilesContext);
 
     const onDrop = useCallback(acceptedFiles => {
         readFiles(acceptedFiles);
+        history.push('/result');
     }, []);
     const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
 
     const handleChange = e => {
         readFiles(e.target.files);
+        History.push('/result');
     };
 
     return (
+        <>
+        {isDragActive ?
         <div {...getRootProps()} className="dragAndDrop">
         <input {...getInputProps()} onChange={handleChange}/>
-        {isDragActive
-            ? <p>Arrastra aquí el archivo ...</p>
-            : <p>Arrastra aquí el archivo, o navega para seleccionarlo</p>
-        }
-        </div>
-    )
+             <p>Suelta aquí el archivo para calcular...</p>
+        </div> 
+        : 
+        <div {...getRootProps()} className=" dragAndDrop dragAndDrop--green">
+        <input {...getInputProps()} onChange={handleChange}/>
+        
+             <p>Arrastra aquí el archivo, o navega para seleccionarlo</p>
+       
+        </div>} 
+        </>
+        )
 }
 
 export default DragAndDrop
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -7,19 +7,11 @@ function Home() {
 
   return (
     <main className="home container">
-      <h1 className="mb-6">Arrastra tu fichero</h1>
+      <h1 className="mb-6">Calcula tu desviación</h1>
       <DragAndDrop></DragAndDrop>
       <br />
       <br />
       
-             <Link  to="/result">
-          <Button 
-          variant="contained" 
-          color="primary" 
-          size="large">    
-            CALCULAR
-          </Button>
-        </Link>
     </main>
   );
 }
